24e0dc45f481b89f41286175c437fab8795a1098
Jake Anhaltzer <j_anhaltzer@u.pacific.edu>
2018-02-17 21:11:19 -0800

    Added weapon attack and health script in the movement testing scene

---
+++ GameWithTheGuys/Assets/Scripts/Player/BasicAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour {
+
+    bool checkForAttack; //Used so you cant start an attack while mid attack
+
+    void Start () {
+        weaponEnabled(false);
+    }
+
+    void FixedUpdate () {
+        checkForAttack = this.GetComponent<BoxCollider2D>().enabled; //Updates if youre in an attack or not
+        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Joystick1Button2)) && checkForAttack == false)
+        {
+            weaponEnabled(true);
+            Invoke("endSwipe", .25f);
+            //play animation of sword swipe here
+        }
+    }
+
+    void endSwipe() //Maybe change this cause its dumb code but it works so
+    {
+        weaponEnabled(false); //Its cause I can't call invoke in update and pass false as the bool while invoking
+    }
+
+    void weaponEnabled(bool swap) //Swaps the weapons sprite and collider. May need to be changed when adding animation
+    {
+        this.GetComponent<BoxCollider2D>().enabled = swap;
+        this.GetComponent<SpriteRenderer>().enabled = swap;
+    }
+
+}
---
+++ GameWithTheGuys/Assets/Scripts/Universal/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    int health;
+
+	// Use this for initialization
+	void Start () {
+        if (this.tag == "Enemy1")
+            health = 4;
+        else if (this.tag == "Player")
+            health = 5;
+	}
+	
+	public void getHit(int damage) //Need to call this from a script that deals with the colliders 
+    {
+        health -= damage;
+        if (health >= 0 && this.tag != "Player")
+            die();
+        else
+            playerDie();
+    }
+
+    void die() 
+    {
+        //Before calling destroy, use this spot to drop any loot such as money or health or anything like that
+        Destroy(this.gameObject);
+    }
+
+    void playerDie()
+    {
+        //Call any special things for player death like a game over screen
+        Destroy(this.gameObject); //Maybe use this
+    }
+}
